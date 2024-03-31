12c831765587b9694cf710eb5a57eddfa133fec7
saadbelgi <114847994+saadbelgi@users.noreply.github.com>
2024-03-31 12:59:45 +0530

    31/3/24 13:00

---
+++ server/misc/script.js
@@ -0,0 +1,330 @@
+obj = [
+    {
+        "id": 1,
+        "name": "Distributed Computing",
+        "code": "DS303",
+        "teacher_id": 5,
+        "students": [
+            {
+                "id": 1,
+                "name": "Anushka Santosh Acharya"
+            },
+            {
+                "id": 2,
+                "name": "Aryaman Agarwal"
+            },
+            {
+                "id": 3,
+                "name": "Manthan Satish Ayalwar"
+            },
+            {
+                "id": 4,
+                "name": "Rohit Shekhar Baviskar"
+            },
+            {
+                "id": 5,
+                "name": "Mohammed Saad M. Ahmad Belgi"
+            },
+            {
+                "id": 6,
+                "name": "Darshit Vikram Bhagtani"
+            },
+            {
+                "id": 7,
+                "name": "Asmi Shailesh Bhanushali"
+            },
+            {
+                "id": 8,
+                "name": "Manav Rajesh Bhanushali"
+            },
+            {
+                "id": 9,
+                "name": "Aditi Nilesh Bhutada"
+            },
+            {
+                "id": 10,
+                "name": "Bhagya Pradeep Bijlaney"
+            },
+            {
+                "id": 11,
+                "name": "Atharva Kailas Bilonikar"
+            },
+            {
+                "id": 12,
+                "name": "Rahul Chandrashekhar Chalwadi"
+            },
+            {
+                "id": 13,
+                "name": "Harsh Tulshidas Chandra"
+            },
+            {
+                "id": 14,
+                "name": "Om Chhaganlal Chandra"
+            },
+            {
+                "id": 15,
+                "name": "Sujal Mahendra Chordia"
+            },
+            {
+                "id": 16,
+                "name": "Vedant Tushar Dapolikar"
+            },
+            {
+                "id": 17,
+                "name": "Janhavi Ram Deshmukh"
+            },
+            {
+                "id": 18,
+                "name": "Omkar Ravikant Deshmukh"
+            },
+            {
+                "id": 19,
+                "name": "Ansari Modh. Modh.Muuzammil"
+            },
+            {
+                "id": 20,
+                "name": "Dsouza Glen Robert"
+            },
+            {
+                "id": 21,
+                "name": "Yash Ravi Dongare"
+            },
+            {
+                "id": 22,
+                "name": "Hrimkar Vishal Doshi"
+            },
+            {
+                "id": 23,
+                "name": "Anish Padmakar Gade"
+            },
+            {
+                "id": 24,
+                "name": "Aditya Sushil Gangal"
+            },
+            {
+                "id": 25,
+                "name": "Sarthak Daulat Gharat"
+            },
+            {
+                "id": 26,
+                "name": "Bodhisatya Ghosh"
+            },
+            {
+                "id": 27,
+                "name": "Abhishek Gupta"
+            },
+            {
+                "id": 28,
+                "name": "Karthik Ganeshan Iyer"
+            },
+            {
+                "id": 29,
+                "name": "Bhushan Sadashiv Jadhav"
+            },
+            {
+                "id": 30,
+                "name": "Khushi Bharat Jain"
+            },
+            {
+                "id": 31,
+                "name": "Rohan Vimal Jaiswal"
+            },
+            {
+                "id": 32,
+                "name": "Yash Rajan Kamble"
+            },
+            {
+                "id": 33,
+                "name": "Tanmay Vivek Kulkarni"
+            },
+            {
+                "id": 34,
+                "name": "Mayank Binod Kumar Kumar"
+            },
+            {
+                "id": 35,
+                "name": "Tushar Prakash Kumare"
+            },
+            {
+                "id": 36,
+                "name": "Pranit Dinesh Lalla"
+            },
+            {
+                "id": 37,
+                "name": "Ghuge Gauri Maruti"
+            },
+            {
+                "id": 38,
+                "name": "Hate Riya Mangesh"
+            },
+            {
+                "id": 39,
+                "name": "Jain Darshan Sanjay"
+            },
+            {
+                "id": 40,
+                "name": "Sanimar Singh Manghera"
+            },
+            {
+                "id": 41,
+                "name": "Anusha Vrajesh Maniar"
+            },
+            {
+                "id": 42,
+                "name": "Ronit Manoj Mankani"
+            },
+            {
+                "id": 43,
+                "name": "Aryan Vinay Nathani"
+            },
+            {
+                "id": 44,
+                "name": "Aditya Nawnath Ningule"
+            },
+            {
+                "id": 45,
+                "name": "Manav Sandesh Padwal"
+            },
+            {
+                "id": 46,
+                "name": "Shriya Sachin Parab"
+            },
+            {
+                "id": 47,
+                "name": "Aarya Prashant Patil"
+            },
+            {
+                "id": 48,
+                "name": "Samruddhi Mahesh Patil"
+            },
+            {
+                "id": 49,
+                "name": "Neemisha Rathindranath Paul"
+            },
+            {
+                "id": 50,
+                "name": "Harshvardhan Balasaheb Pawar"
+            },
+            {
+                "id": 51,
+                "name": "Sakshi Malhari Phapale"
+            },
+            {
+                "id": 52,
+                "name": "Nikhil Ramashray Prajapati"
+            },
+            {
+                "id": 53,
+                "name": "Rashmi Samarjit Rajeshirke"
+            },
+            {
+                "id": 54,
+                "name": "Suraj Omprakash Rathi"
+            },
+            {
+                "id": 55,
+                "name": "Nitesh Bhaskar Rathod"
+            },
+            {
+                "id": 56,
+                "name": "Mestry Devang Haresh"
+            },
+            {
+                "id": 57,
+                "name": "Phansopkar Omkar Gajanan"
+            },
+            {
+                "id": 58,
+                "name": "Rodrigues Kenneth David"
+            },
+            {
+                "id": 59,
+                "name": "Divya Lalitraj Suvarna"
+            },
+            {
+                "id": 60,
+                "name": "Advait Ravi Sapkal"
+            },
+            {
+                "id": 61,
+                "name": "Rupali Rajendra Sawale"
+            },
+            {
+                "id": 62,
+                "name": "Darsh Mihir Shah"
+            },
+            {
+                "id": 63,
+                "name": "Kinnari Ketan Shah"
+            },
+            {
+                "id": 64,
+                "name": "Maaz Imtiyaz Shaikh"
+            },
+            {
+                "id": 65,
+                "name": "Atul Sharma"
+            },
+            {
+                "id": 66,
+                "name": "Ritik Singh"
+            },
+            {
+                "id": 67,
+                "name": "Vasundhara Singh"
+            },
+            {
+                "id": 68,
+                "name": "Siddhesh Anand Sonar"
+            },
+            {
+                "id": 69,
+                "name": "Smruti Rajesh Sonekar"
+            },
+            {
+                "id": 70,
+                "name": "Loukik Sainath Tayshete"
+            },
+            {
+                "id": 71,
+                "name": "Ronak Satish Thakar"
+            },
+            {
+                "id": 72,
+                "name": "Rishi Tiku"
+            },
+            {
+                "id": 73,
+                "name": "Anand Tiwari"
+            },
+            {
+                "id": 74,
+                "name": "Deepika Alok Kumar Trivedi"
+            },
+            {
+                "id": 75,
+                "name": "Stephen David Vaz"
+            },
+            {
+                "id": 76,
+                "name": "Shubham Mahendrakumar Vishwakarma"
+            },
+            {
+                "id": 77,
+                "name": "Vedanti Anil Wadatkar"
+            },
+            {
+                "id": 78,
+                "name": "Satere Nishant Dinesh"
+            },
+            {
+                "id": 79,
+                "name": "Siddiqui Hawaiza Sabeel Mohd Sultan"
+            }
+        ]
+    }
+]
+let y = obj[0].students.map((x) => x.name)
+for(let i = 0; i < y.length; i++) {
+    console.log(y[i])
+}
